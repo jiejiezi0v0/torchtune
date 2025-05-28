0906fc2f5ae9094115c9ebc087a9ab349d9eb84d
jiejiezi0v0 <108184153+jiejiezi0v0@users.noreply.github.com>
2025-05-28 10:59:39 +0800

    Update __init__.py

--- torchtune/data/__init__.py
+++ torchtune/data/__init__.py
@@ -4,6 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+from torchtune.data._chat_formats import ChatFormat
 from torchtune.data._collate import (
     left_pad_sequence,
     padded_collate,
@@ -13,11 +14,13 @@ from torchtune.data._collate import (
     padded_collate_tiled_images_and_mask,
 )
 from torchtune.data._common import CROSS_ENTROPY_IGNORE_IDX
+from torchtune.data._converters import get_openai_messages, get_sharegpt_messages, arc_to_messages
+from torchtune.data._instruct_templates import InstructTemplate
 from torchtune.data._messages import (
+    ARCToMessages,
     AlpacaToMessages,
     ChosenRejectedToMessages,
     InputOutputToMessages,
-    mask_messages,
     Message,
     OpenAIToMessages,
     Role,
@@ -35,16 +38,18 @@ from torchtune.data._prompt_templates import (
 from torchtune.data._utils import format_content_with_images, load_image, truncate
 
 __all__ = [
+    "ChatFormat",
     "CROSS_ENTROPY_IGNORE_IDX",
     "GrammarErrorCorrectionTemplate",
+    "InstructTemplate",
     "SummarizeTemplate",
     "OpenAIToMessages",
     "ShareGPTToMessages",
     "AlpacaToMessages",
+    "ARCToMessages",
     "truncate",
     "Message",
     "validate_messages",
-    "mask_messages",
     "Role",
     "format_content_with_images",
     "PromptTemplateInterface",
@@ -53,6 +58,9 @@ __all__ = [
     "ChosenRejectedToMessages",
     "QuestionAnswerTemplate",
     "ChatMLTemplate",
+    "get_openai_messages",
+    "get_sharegpt_messages",
+    "arc_to_messages",
     "padded_collate_sft",
     "padded_collate_dpo",
     "left_pad_sequence",
