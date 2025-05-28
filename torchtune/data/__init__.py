3c4eb7263fdccd5a24d3eb4dec47a9b4d37c56e3
jiejiezi0v0 <108184153+jiejiezi0v0@users.noreply.github.com>
2025-05-28 11:05:19 +0800

    Update __init__.py

--- torchtune/data/__init__.py
+++ torchtune/data/__init__.py
@@ -14,7 +14,7 @@ from torchtune.data._collate import (
     padded_collate_tiled_images_and_mask,
 )
 from torchtune.data._common import CROSS_ENTROPY_IGNORE_IDX
-from torchtune.data._messages import get_openai_messages, get_sharegpt_messages, arc_to_messages
+from torchtune.data._converters import get_openai_messages, get_sharegpt_messages, arc_to_messages
 from torchtune.data._instruct_templates import InstructTemplate
 from torchtune.data._messages import (
     ARCToMessages,
